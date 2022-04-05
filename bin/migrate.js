3b69392511822f1921e19feeb4bb3e5bcb855687
wengjq <wengjq@mingyuanyun.com>
2022-04-05 10:19:34 +0800

    update

--- bin/migrate.js
+++
@@ -1,7 +0,0 @@
-const models = require('../app/models');
-
-models.sequelize.sync().done(() => {
-   console.log('sync db done and waiting for 1 minitue to exit,Or CTRL+C to exit.');
-},(err) => {
-   console.log(err);
-});
